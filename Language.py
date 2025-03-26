5c0b5ae84779618270853495dbe5704ff441214f
Fl0ris1 <149056628+Fl0ris1@users.noreply.github.com>
2025-03-26 20:53:09 +0100

    Update Language.py

--- Language.py
+++ Language.py
@@ -6,10 +6,10 @@ added_var=""
 var_num=0
 instructions=[]
 math_tokens=["+","-","*","/"]
-print_tokens=["SayStr","SayInt"]
-var_tokens=["VarAdd","VarSay","VarDel"]
+print_tokens=["SayStr","SayInt","Say"]
+var_tokens=["VarAdd","VarSay","VarDel","Var"]
 console_commands=["cc","clear"]
-all_tokens=[math_tokens+print_tokens+var_tokens+console_commands]
+all_tokens=math_tokens+print_tokens+var_tokens+console_commands
 
 """class Vars:
     def __init__(self):"""
@@ -18,88 +18,103 @@ while True:
     inp=input("LANGUAGE >")
     tokens=inp.split()
     print(tokens)
-
+    
+    #if len(tokens)>1:
+    if tokens[0] in all_tokens or tokens[1] in all_tokens:
     #check if tokens used for math
-    if tokens[1] in math_tokens:
+        if tokens[1] in math_tokens:
 
-        if tokens[1]=="+":
-            x=int(tokens[0])+int(tokens[2])
+            if tokens[1]=="+":
+                x=int(tokens[0])+int(tokens[2])
 
-        elif tokens[1]=="-":
-            x=int(tokens[0])-int(tokens[2])
+            elif tokens[1]=="-":
+                x=int(tokens[0])-int(tokens[2])
 
-        elif tokens[1]=="*":
-            x=int(tokens[0])*int(tokens[2])
+            elif tokens[1]=="*":
+                x=int(tokens[0])*int(tokens[2])
 
-        elif tokens[1]=="/":
-            x=int(tokens[0])/int(tokens[2])
+            elif tokens[1]=="/":
+                x=int(tokens[0])/int(tokens[2])
 
     #checks if tokens used for saying
-    elif tokens[0] in print_tokens:
+        elif tokens[0]=="Say":
 
-        if tokens[0]==print_tokens[0]:
-            tokens.remove(tokens[0])
-            for i in range(0,len(tokens)):
-                print(tokens[i]," ",end="")
-            print("")
+            if tokens[1]=="Str":
+                for i in range(2,len(tokens)):
+                    print(tokens[i]," ",end="")
+                print("")
 
-        elif tokens[0]==print_tokens[1] and tokens[2] in math_tokens:
+            elif tokens[1]=="Int" and tokens[3] in math_tokens:
 
-            if tokens[2]=="+":
-                x=int(tokens[1])+int(tokens[3])
+                if tokens[3]=="+":
+                    x=int(tokens[2])+int(tokens[4])
 
 
-            elif tokens[2]=="-":
-                x=int(tokens[1])-int(tokens[3])
+                elif tokens[3]=="-":
+                    x=int(tokens[2])-int(tokens[4])
             
 
-            elif tokens[2]=="*":
-                x=int(tokens[1])*int(tokens[3])
+                elif tokens[3]=="*":
+                    x=int(tokens[2])*int(tokens[4])
             
 
-            elif tokens[2]=="/":
-                x=int(tokens[1])/int(tokens[3])
+                elif tokens[3]=="/":
+                    x=int(tokens[2])/int(tokens[4])
         
-            print(x)
+                print(x)
+
+            elif tokens[1]=="Var":
+                if tokens[2]=="All":
+                    print(var)
+
+                elif tokens[2].isdigit():
+                    if int(tokens[2])>len(var)-1:
+                        print("LIST INDEX OUT OF RANGE")
+
+                    else:
+                        print(var[int(tokens[2])])
 
     #used for console commands
-    elif tokens[0]=="cc" :
-        if tokens[1]=="clear":
-            os.system('cls')
+        elif tokens[0]=="cc" :
+            if tokens[1]=="clear":
+                os.system('cls')
 
-    elif tokens[0] in var_tokens:
-        if tokens[0]=="VarAdd":
+        elif tokens[0] in var_tokens:
+            if tokens[0]=="Var":
+                if tokens[1]=="Add":
+                    for i in range(2,len(tokens)):
+                        if i==len(tokens)-1:
+                            added_var=added_var+tokens[i]
+                        else:
+                            added_var=added_var+tokens[i]+" "
 
-            for i in range(1,len(tokens)):
-                if i==len(tokens)-1:
-                    added_var=added_var+tokens[i]
-                else:
-                    added_var=added_var+tokens[i]+" "
+                    var.append(added_var)
+                    print(var[var_num])
+                    added_var=""
+                    var_num+=1
 
-            var.append(added_var)
-            print(var[var_num])
-            added_var=""
-            var_num+=1
-
-        elif tokens[0]=="VarSay":
-            if tokens[1]=="All":
-                print(var)
-
-            else:
-                if int(tokens[1])>len(var)-1:
-                    print("LIST INDEX OUT OF RANGE")
-
-                else:
-                    print(var[int(tokens[1])])
-
-        elif tokens[0]=="VarDel":
-            if int(tokens[1])>len(var)-1:
-                print("UNABLE TO REMOVE NON-EXISTING OBJECT")
+                elif tokens[1]=="Del":
+                    if tokens[2].isdigit():
+                        if int(tokens[2])>len(var)-1:
+                            print("UNABLE TO REMOVE NON-EXISTING OBJECT")
+                    
+                        else:
+                            var.remove(var[int(tokens[2])])
+                            var_num-=1
+                
+                
+                    elif tokens[2]=="All":
+                        var.clear()
+                        var_num=0
             
+                
+                else:
+                    print("SYNTAX ERROR")
             else:
-                var.remove(var[int(tokens[1])])
-                var_num-=1
-
+                print("SYNTAX ERROR")
+        else:
+            print("SYNTAX ERROR")
+    
     else:
         print("SYNTAX ERROR")
 
